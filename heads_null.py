154369786fafe8131e15c0aa6eb107677517534a
lana-svet <svetlana.volskaya@gmail.com>
2014-05-14 03:12:19 +0600

    finding heads for simple groups of 1 token and complex groups of 1 group

--- heads_null.py
+++ heads_null.py
@@ -2,11 +2,28 @@ import sys
 sys.path.append('/corpus/python')
 from Annotation import AnnotationEditor
 editor = AnnotationEditor('config.ini')
-editor.db_cursor.execute('SELECT group_id FROM syntax_groups WHERE head_id = 0')
+
+# определение вершин простых групп из 1 токена
+editor.db_cursor.execute('SELECT syntax_groups_simple.group_id, token_id, COUNT(token_id) AS tki FROM syntax_groups_simple INNER JOIN syntax_groups ON syntax_groups_simple.group_id = syntax_groups.group_id WHERE head_id = 0 GROUP BY group_id HAVING tki = 1')
+result_simple = editor.db_cursor.fetchall()
+
+for row in result_simple:
+    editor.db_cursor.execute('UPDATE syntax_groups SET head_id = ' + str(row['token_id']) + ' WHERE group_id = ' + str(row['group_id']))
+
+
+# определение вершин сложных групп из 1 ИГ  
+editor.db_cursor.execute('SELECT parent_gid, child_gid, COUNT(child_gid) AS chg FROM syntax_groups_complex INNER JOIN syntax_groups ON syntax_groups_complex.parent_gid = syntax_groups.group_id WHERE head_id = 0 GROUP BY group_id HAVING chg = 1')
+result_complex = editor.db_cursor.fetchall()
+
+for row in result_complex:
+    editor.db_cursor.execute('UPDATE syntax_groups SET head_id = ' + str(row['child_gid']) + ' WHERE group_id = ' + str(row['parent_gid']))
+
+    
+# вывод вершин в файл    
+editor.db_cursor.execute('SELECT group_id, head_id FROM syntax_groups')
 result = editor.db_cursor.fetchall()
 
-fd = open('result_heads_null.txt', 'w')
+fd = open('syntax_groups.txt', 'w')
 for row in result:
-    fd.write(str(row['group_id']) + "\n")
+    fd.write(', '.join("%s = %s" % (k,v) for (k,v) in row.iteritems()) + "\n")
 fd.close()
-print(result)
